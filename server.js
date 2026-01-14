6eaea87c5298abbc2034dcde2fe822eee3b97109
Yaroslav-Simonenko <simonenkoyaroslav2008@gmail.com>
2026-01-14 16:48:19 +0200

    recorder

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public')); // 🔥 Дозволяємо доступ до recorder.html
 
 // Firebase Service Account
 let serviceAccount;
